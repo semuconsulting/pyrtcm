7951efa03d9f0ddbe00d025b7d3346feb2629397
semuadmin <28569967+semuadmin@users.noreply.github.com>
2024-12-03 10:08:40 +0000

    refresh ntrip client example

--- examples/rtcm_ntrip_client.py
+++ examples/rtcm_ntrip_client.py
@@ -11,7 +11,7 @@ or passed as keyword arguments user and password.
 
 Usage:
 
-python3 rtcm_ntrip_client.py server="yourcaster" mountpoint="yourmountpoint" user="youruser" password="yourpassword" outfile="rtcmntrip.log"
+python3 rtcm_ntrip_client.py server="yourcaster" port=2101 mountpoint="yourmountpoint" user="youruser" password="yourpassword" outfile="rtcmntrip.log"
 
 Run from /examples folder.
 
@@ -22,16 +22,14 @@ Created on 12 Feb 2023
 :license: BSD 3-Clause
 """
 
+from logging import getLogger
 from os import getenv
 from sys import argv
 from time import sleep
 
-from pygnssutils import GNSSNTRIPClient
+from pygnssutils import VERBOSITY_HIGH, GNSSNTRIPClient, set_logging
 
-PORT = 2101
-HTTPS = 0
-if PORT == 443:
-    HTTPS = 1
+DEFAULT_PORT = 2101
 
 
 def main(**kwargs):
@@ -39,7 +37,10 @@ def main(**kwargs):
     Main routine.
     """
 
+    logger = getLogger("pygnssutils.gnssntripclient")
+    set_logging(logger, VERBOSITY_HIGH)
     server = kwargs.get("server", "rtk2go.com")
+    port = int(kwargs.get("port", DEFAULT_PORT))
     mountpoint = kwargs.get("mountpoint", "")
     user = kwargs.get("user", getenv("PYGPSCLIENT_USER", "user"))
     password = kwargs.get("password", getenv("PYGPSCLIENT_PASSWORD", "password"))
@@ -48,13 +49,15 @@ def main(**kwargs):
     with open(outfile, "wb") as out:
         gnc = GNSSNTRIPClient()
 
-        print(
+        https = 1 if port == 443 else 0
+
+        logger.info(
             f"RTCM NTRIP Client started, writing output to {outfile}... Press CTRL-C to terminate."
         )
         gnc.run(
             server=server,
-            port=PORT,
-            https=HTTPS,
+            port=port,
+            https=https,
             mountpoint=mountpoint,
             datatype="RTCM",
             ntripuser=user,
@@ -67,7 +70,7 @@ def main(**kwargs):
             while True:
                 sleep(3)
         except KeyboardInterrupt:
-            print("RTCM NTRIP Client terminated by User")
+            logger.info("RTCM NTRIP Client terminated by User")
 
 
 if __name__ == "__main__":
